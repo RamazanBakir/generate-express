62b619dbc684450620937600a68cab4e8ee56ddc
Ramazan Bakır <48602486+RamazanBakir@users.noreply.github.com>
2020-12-14 15:11:37 +0300

    MongoDB operations
    In this commit shown as a some CRUD operations and output results.

--- conFusionServer/node-mongodb/index.js
+++ conFusionServer/node-mongodb/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper= require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -9,25 +10,62 @@ MongoClient.connect(url,(err,client) => {
 
     console.log('Connected correctly to server');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
 
-    collection.insertOne({"name":"RamazanB","description":"Test"},(err,result)=>{
-        assert.equal(err,null);
-
-        console.log('After Insert:\n');
-        console.log(result.ops);
-
-        collection.find({}).toArray((err,docs)=>{
-            assert.equal(err,null);
-
-            console.log('Found : \n');
-            console.log(docs);
-
-            db.dropCollection('dishes',(err,result)=>{
-                assert.equal(err,null);
+    dboper.insertDocument(db,{name:"Hasan",description:'Deneme'},'dishes',(result)=>{
+        console.log('Insert Document : \n',result.ops);
+        
+        dboper.findDocuments(db,'dishes',(docs)=>{
+            console.log('Found Documents: \n',docs);
+            
+            dboper.updateDocument(db,{name:'Hasan'},{description:'Update desc.'},'dishes',(result)=>{
+                console.log('Updated Document : \n',result.result);
                 
-                client.close();
+                dboper.findDocuments(db,'dishes',(docs)=>{
+                    console.log('Found Documents: \n',docs);
+
+                    db.dropCollection('dishes',(result) =>{
+                        console.log('Drop Collection is :\n',result);
+
+                        client.close();
+                    });
+                });
             });
         });
     });
 });
+
+/* output is like that : 
+
+Connected correctly to server
+Inserted 1 documents into the collection dishes
+Insert Document :
+ [
+  {
+    name: 'Hasan',
+    description: 'Deneme',
+    _id: 5fd755dbe6cc180d500fe772
+  }
+]
+Found Documents:
+ [
+  {
+    _id: 5fd755dbe6cc180d500fe772,
+    name: 'Hasan',
+    description: 'Deneme'
+  }
+]
+Updated the document with  { description: 'Update desc.' }
+Updated Document :
+ { n: 1, nModified: 1, ok: 1 }
+Found Documents:
+ [
+  {
+    _id: 5fd755dbe6cc180d500fe772,
+    name: 'Hasan',
+    description: 'Update desc.'
+  }
+]
+Drop Collection is :
+ null
+
+ */
